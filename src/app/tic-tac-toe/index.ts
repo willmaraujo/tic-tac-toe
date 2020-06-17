12169e75dd0264d1f36102f2db594a53b09beec0
Williams Araujo <crfwilliams@gmail.com>
2020-06-17 16:40:32 +0100

    Added tic-tac-toe service
    Signed-off-by: Williams Araujo <crfwilliams@gmail.com>

--- src/app/tic-tac-toe/index.ts
+++ src/app/tic-tac-toe/index.ts
@@ -1,2 +1,4 @@
 export * from './tic-tac-toe.module';
 export * from './tic-tac-toe.component';
+export * from './shared';
+
--- src/app/tic-tac-toe/tic-tac-toe.module.ts
+++ src/app/tic-tac-toe/tic-tac-toe.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TicTacToeComponent } from './tic-tac-toe.component';
+import { TicTacToeService } from './shared';
 
 
 @NgModule({
@@ -10,6 +11,9 @@ import { TicTacToeComponent } from './tic-tac-toe.component';
   ],
   exports: [
     TicTacToeComponent
+  ],
+  providers: [
+    TicTacToeService
   ]
 })
 export class TicTacToeModule { }
