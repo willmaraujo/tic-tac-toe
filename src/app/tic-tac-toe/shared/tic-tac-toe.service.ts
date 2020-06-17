fb4e42cdd3d1439d0b055828a865d9afc7811754
Williams Araujo <crfwilliams@gmail.com>
2020-06-17 17:42:18 +0100

    Start to implement service

--- src/app/tic-tac-toe/shared/tic-tac-toe.service.ts
+++ src/app/tic-tac-toe/shared/tic-tac-toe.service.ts
@@ -5,5 +5,33 @@ import { Injectable } from '@angular/core';
 })
 export class TicTacToeService {
 
+  private readonly TAM_TAB: number = 3;
+  private readonly X: number = 1;
+  private readonly O: number = 2;
+  private readonly EMPTY: number = 0;
+
+  private board: any;
+  private countMoves: number;
+  private win: any;
+
+  private _player: number;
+  private _showStart: boolean;
+  private _showBoard: boolean;
+  private _showEnd: boolean;
+
   constructor() { }
+
+  initGame(): void {
+    this._showStart = true;
+    this._showBoard = false;
+    this._showEnd = false;
+    this.countMoves = 0;
+    this._player = this.X;
+    this.win = false;
+    this.initBoard();
+  }
+
+  initBoard(): void {
+
+  }
 }
