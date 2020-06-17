fa01ae80e74edd0cde17eb6136c338d1f54fb5ff
Williams Araujo <crfwilliams@gmail.com>
2020-06-17 16:31:05 +0100

    Created tic-tac-toe component

--- src/app/tic-tac-toe/index.ts
+++ src/app/tic-tac-toe/index.ts
@@ -1,1 +1,2 @@
 export * from './tic-tac-toe.module';
+export * from './tic-tac-toe.component';
--- src/app/tic-tac-toe/tic-tac-toe.module.ts
+++ src/app/tic-tac-toe/tic-tac-toe.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TicTacToeComponent } from './tic-tac-toe.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [TicTacToeComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    TicTacToeComponent
   ]
 })
 export class TicTacToeModule { }
