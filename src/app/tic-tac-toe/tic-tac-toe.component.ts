70c07c604da8ad4f9fe9101596f8b76b965072e4
Williams Araujo <crfwilliams@gmail.com>
2020-07-01 17:47:08 +0100

    Implemented initial flow
    Signed-off-by: Williams Araujo <crfwilliams@gmail.com>

--- src/app/tic-tac-toe/tic-tac-toe.component.ts
+++ src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TicTacToeService } from './shared/tic-tac-toe.service';
 
 @Component({
   selector: 'app-tic-tac-toe',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicTacToeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ticTacToeService: TicTacToeService) { }
 
   ngOnInit(): void {
+    this.ticTacToeService.init();
+  }
+
+  initGame(): void {
+    this.ticTacToeService.initGame();
+  }
+
+  get showStart(): boolean {
+    return this.ticTacToeService.showStart;
+  }
+
+  get showBoard(): boolean {
+    return this.ticTacToeService.showBoard;
+  }
+
+  get showEnd(): boolean {
+    return this.ticTacToeService.showEnd;
   }
 
 }
